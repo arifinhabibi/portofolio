21b14ec967994e08d30cc9b074c6beca3e460279
Muhammad Arifin Habibi <arifinhabibi76@gmail.com>
2023-10-02 09:41:45 +0700

    Initial commit

---
+++ src/components/SidebarComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SidebarComponent() {
+  return (
+    <div>
+      <h1 className="text-2xl font-mono underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default SidebarComponent;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarComponent from "../components/SidebarComponent";
+
+function Homepage() {
+  return (
+    <div>
+      <SidebarComponent />
+    </div>
+  );
+}
+
+export default Homepage;
